7b8639d388c11ea91729fc67d721ad45c924d8a5
William Pereira <pereirawe@gmail.com>
2022-03-19 06:49:46 -0300

    first commit

---
+++ npm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm init -y
+
+# Cliente para Whatsapp
+npm install --save whatsapp-web.js
+
+# Gera um QR dentro do terminal
+npm install --save qrcode-terminal
+
+# Cria nossa API
+npm install --save express
+
+# Libera os CORS para a API
+npm install --save cors
